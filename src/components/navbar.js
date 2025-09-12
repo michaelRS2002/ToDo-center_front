9cde913fbf8bff256665543b17d0a94e91b2680f
juanpab05 <165406198+juanpab05@users.noreply.github.com>
2025-09-12 13:20:21 -0500

    added / to Navbar src
    En la pagina profile/edit no cargaba la foto del navbar por esto

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ export default function Navbar() {
   return `<div class="head">
         <div class="logo">
             <a href="#">
-                <img src="public/images/icon.png" alt="Logo">
+                <img src="/public/images/icon.png" alt="Logo">
             </a>
         </div>
         <nav class="navbar">
