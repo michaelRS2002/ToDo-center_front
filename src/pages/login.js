2ea354786ca4f2c4b011450167a58a6ffa76bad3
michaelRS2002 <ramirez.michael@correounivalle.edu.co>
2025-09-16 01:10:31 -0500

    add conexion with back in login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -35,7 +35,7 @@ export default function Login() {
       return;
     }
     try {
-      const response = await fetch('http://localhost:3000/api/auth/login', {
+      const response = await fetch('https://todo-center-back.onrender.com/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ correo: email, contrasena: password })
